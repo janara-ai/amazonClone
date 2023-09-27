4b976b554286d017c5fc6547815c678b6b56623c
janara-ai <jane1998.pls@gmail.com>
2023-09-27 21:00:30 +0400

    implemented header bottom

--- src/components/header/HeaderBottom.jsx
+++ src/components/header/HeaderBottom.jsx
@@ -1,8 +1,91 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import { useState } from "react";
+import { motion } from "framer-motion";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import SideNavContent from "./SideNavContent";
 
 const HeaderBottom = () => {
+  const ref = useRef();
+  const [sideBar, setSideBar] = useState(false);
+  useEffect(() => {
+    document.body.addEventListener("click", (e) => {
+      if (e.target.contains(ref.current)) {
+        setSideBar(false);
+      }
+    });
+  }, [ref, sideBar]);
   return (
-    <div className="w-full px-4 h-[36px] flex items-center bg-amazon_light text-white"></div>
+    <div className="w-full px-4 h-[36px] flex items-center bg-amazon_light text-white">
+      <ul className="flex items-center gap-2 text-sm  tracking-wide">
+        <li
+          onClick={() => setSideBar(true)}
+          className="headerHover flex items-center gap-1"
+        >
+          <MenuIcon />
+          All
+        </li>
+        <li className="headerHover"> Today's Deals</li>
+        <li className="headerHover">Customer Service</li>
+        <li className="headerHover">Gift Cards</li>
+        <li className="headerHover">Registry</li>
+        <li className="headerHover">Sell</li>
+      </ul>
+
+      {sideBar && (
+        <div className="w-full h-screen text-black fixed top-0 left-0 bg-amazon_blue bg-opacity-50">
+          <div className="w-full h-full relative">
+            <motion.div
+              ref={ref}
+              initial={{ x: -500, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ duration: 0.5 }}
+              className=" w-[290px] md:w-[350px] h-full bg-white border border-black"
+            >
+              <div className="w-full bg-amazon_light text-white py-2 px-6 flex items-center gap-4">
+                <AccountCircleIcon />
+                <h3 className="font-titleFont font-bold text-lg tracking-wide">
+                  Hello, Sign In{" "}
+                </h3>
+              </div>
+
+              <SideNavContent
+                title="Digital Content & Devices"
+                one="Amazon music"
+                two="Kindle E-readers & Books"
+                three="Amazon Appstore"
+              />
+              <SideNavContent
+                title="Shop by Department"
+                one="Electronic"
+                two="Computer"
+                three="Smart Home"
+              />
+              <SideNavContent
+                title="Programs & Features"
+                one="Gift Cards"
+                two="Amazon live"
+                three="International Shopping"
+              />
+              <SideNavContent
+                title="Help & Settings"
+                one="Your Account"
+                two="Computer Service"
+                three="Contact us"
+              />
+
+              <span
+                onClick={() => setSideBar(false)}
+                className="cursor-pointer absolute top-0 left-[350px] w-10 h-11 text-black flex items-center justify-center border bg-gray-200 hover:bg-red-500 hover:text-white duration-300"
+              >
+                <CloseIcon />
+              </span>
+            </motion.div>
+          </div>
+        </div>
+      )}
+    </div>
   );
 };
 
